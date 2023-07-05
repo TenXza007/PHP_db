3792b7076d07b4e8f5389af9b17f37a8dbfc811d
Ten <6421061012@cdti.ac.th>
2023-07-06 01:31:05 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "cdti1234";
+$dbname = "db_ten";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
